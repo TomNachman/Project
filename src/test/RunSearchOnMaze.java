8c70a0ec5c29d818fe8e7aad43704bf3b03e106b
asafsalo <3asafi51@gmail.com>
2020-03-30 15:05:40 +0300

    Part A: Completed Part B: Completed - DepthFirstSearch, BreadthFirstSearch         TODO      - BestFirstSearch, Exceptions, Check null

--- src/test/RunSearchOnMaze.java
+++ src/test/RunSearchOnMaze.java
@@ -7,19 +7,18 @@ import java.util.ArrayList;
 public class RunSearchOnMaze {
     public static void main(String[] args) {
         IMazeGenerator mg = new MyMazeGenerator();
-        Maze maze = mg.generate(5, 5);
-
+        Maze maze = mg.generate(500, 500);
         maze.print();
+
         SearchableMaze searchableMaze = new SearchableMaze(maze);
+        solveProblem(searchableMaze, new DepthFirstSearch());
         solveProblem(searchableMaze, new BreadthFirstSearch());
-        long startTime = System.currentTimeMillis();
-        //solveProblem(searchableMaze, new DepthFirstSearch());
-        long endTime = System.currentTimeMillis();
-        System.out.println(String.format("Time in Seconds: %d", (endTime - startTime)/1000));
         //solveProblem(searchableMaze, new BestFirstSearch());
+
     }
     private static void solveProblem(ISearchable domain, ISearchingAlgorithm
             searcher) {
+        long startTime = System.currentTimeMillis();
         //Solve a searching problem with a searcher
         Solution solution = searcher.solve(domain);
         System.out.println(String.format("'%s' algorithm - nodes evaluated:%s", searcher.getName(), searcher.getNumberOfNodesEvaluated()));
@@ -29,5 +28,9 @@ public class RunSearchOnMaze {
         for (int i = 0; i < solutionPath.size(); i++) {
             System.out.println(String.format("%s. %s",i,solutionPath.get(i)));
         }
+        long endTime = System.currentTimeMillis();
+        System.out.println(String.format("'%s' algorithm - nodes evaluated:%s", searcher.getName(), searcher.getNumberOfNodesEvaluated()));
+        System.out.println(String.format("Time in Seconds: %d", (endTime - startTime)/1000));
+
     }
 }
