575a90e6b16e8c975a897f8e78a47e30fd4883c2
asafsalo <3asafi51@gmail.com>
2020-03-30 20:04:25 +0300

    Part A: Completed Part B: Completed - DepthFirstSearch, BreadthFirstSearch         TODO      - BestFirstSearch, Exceptions, Check null

--- src/test/RunSearchOnMaze.java
+++ src/test/RunSearchOnMaze.java
@@ -1,37 +1,41 @@
 package test;
+
 import algorithms.mazeGenerators.IMazeGenerator;
 import algorithms.mazeGenerators.Maze;
 import algorithms.mazeGenerators.MyMazeGenerator;
 import algorithms.search.*;
-import java.util.ArrayList;
-public class RunSearchOnMaze {
 
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.PriorityQueue;
+import java.util.Queue;
+
+/**
+ * Created by Aviadjo on 3/22/2017.
+ */
+public class RunSearchOnMaze {
     public static void main(String[] args) {
         IMazeGenerator mg = new MyMazeGenerator();
-        Maze maze = mg.generate(1000, 1000);
+        Maze maze = mg.generate(5, 5);
+        SearchableMaze searchableMaze = new SearchableMaze(maze);
         maze.print();
 
-        SearchableMaze searchableMaze = new SearchableMaze(maze);
-        solveProblem(searchableMaze, new DepthFirstSearch());
         solveProblem(searchableMaze, new BreadthFirstSearch());
+        //solveProblem(searchableMaze, new DepthFirstSearch());
         solveProblem(searchableMaze, new BestFirstSearch());
-
     }
-    private static void solveProblem(ISearchable domain, ISearchingAlgorithm
-            searcher) {
-        long startTime = System.currentTimeMillis();
-        //Solve a searching problem with a searcher 
+
+    private static void solveProblem(ISearchable domain, ISearchingAlgorithm searcher) {
+        //Solve a searching problem with a searcher
         Solution solution = searcher.solve(domain);
-        System.out.println(String.format("'%s' algorithm - nodes evaluated:%s", searcher.getName(), searcher.getNumberOfNodesEvaluated()));
-                //Printing Solution Path
-                System.out.println("Solution path:");
+        System.out.println(String.format("'%s' algorithm - nodes evaluated: %s", searcher.getName(), searcher.getNumberOfNodesEvaluated()));
+        //Printing Solution Path
+        System.out.println("Solution path:");
         ArrayList<AState> solutionPath = solution.getSolutionPath();
         for (int i = 0; i < solutionPath.size(); i++) {
             System.out.println(String.format("%s. %s",i,solutionPath.get(i)));
         }
-        long endTime = System.currentTimeMillis();
-        System.out.println(String.format("'%s' algorithm - nodes evaluated:%s", searcher.getName(), searcher.getNumberOfNodesEvaluated()));
-        System.out.println(String.format("Time in Seconds: %d", (endTime - startTime)/1000));
     }
+
 
 }
