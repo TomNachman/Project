4ae6b14abb84597fb5a6d3705686b2b42f3e0f1a
tom <nachmant@post.bgu.ac.il>
2020-04-01 11:27:40 +0300

    Added JUnit Tests

---
+++ src/JUnitNew/JUnitTestingBestFirstSearch.java
@@ -0,0 +1,57 @@
+package JUnitNew;
+
+import algorithms.mazeGenerators.EmptyMazeGenerator;
+import algorithms.mazeGenerators.IMazeGenerator;
+import algorithms.mazeGenerators.Maze;
+import algorithms.search.*;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+
+class JUnitTestingBestFirstSearch {
+    //Checking Null input
+    @Test
+    public void NullTest(){
+        ISearchingAlgorithm Best = new BestFirstSearch();
+        Assertions.assertNull(Best.solve(null));
+    }
+    //Checking getName method
+    @Test
+    public void NameTest(){
+        ISearchingAlgorithm Best = new BestFirstSearch();
+        Assertions.assertEquals("BestFirstSearch",Best.getName());
+    }
+    //Checking BestFS on empty Maze(should be always the diagonal)
+    @Test
+    public void EmptyMazeTest(){
+        ISearchingAlgorithm Best = new BestFirstSearch();
+        IMazeGenerator mg = new EmptyMazeGenerator();
+        Maze maze;
+        int count = 0;
+        for (int i=10;i<=100;i+=10){
+            maze = mg.generate(i, i);
+            SearchableMaze searchableMaze = new SearchableMaze(maze);
+            Solution solution = Best.solve(searchableMaze);
+            ArrayList<AState> solutionPath = solution.getSolutionPath();
+            if (solutionPath.size() == i) count++;
+        }
+        Assertions.assertEquals(10,count);
+    }
+    //Checking BestFS for a Maze Without a Solution
+    @Test
+    public void NoSolutionTest() {
+        ISearchingAlgorithm Best = new BestFirstSearch();
+        IMazeGenerator mg = new EmptyMazeGenerator();
+        Maze maze = mg.generate(5,5);
+        maze.MakeWall(4,3);
+        maze.MakeWall(3,4);
+        SearchableMaze searchableMaze = new SearchableMaze(maze);
+        Solution solution = Best.solve(searchableMaze);
+        Solution EmptySolution = new Solution();
+        Assertions.assertEquals(EmptySolution.getSolutionPath().isEmpty(),solution.getSolutionPath().isEmpty());
+    }
+
+
+
+}
