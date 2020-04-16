433fee3a7e6ce0b4c4ee79f822c507abcb1cf140
tom <nachmant@post.bgu.ac.il>
2020-04-16 17:00:28 +0300

    Added Serializable implementations to needed classes Update client and server packeges need to finish ServerStrategyGenerateMaze need to add threds shit to server

--- src/Server/ServerStrategyGenerateMaze.java
+++ src/Server/ServerStrategyGenerateMaze.java
@@ -10,17 +10,22 @@ import algorithms.mazeGenerators.MyMazeGenerator;
 public class ServerStrategyGenerateMaze implements IServerStrategy {
 
     @Override
+    // ראיתי באיזה סרטון ששמים כאן synchronized בהקשר הthreads אבל אין לי מושג עדיין
     public void serverStrategy(InputStream inFromClient, OutputStream outToClient) throws FileNotFoundException {
         try{
-            int [] MazeDim = (int[])inFromClient.read(); //need to solve
+            ObjectInputStream DimInputStream = new ObjectInputStream(inFromClient);
+            ObjectOutputStream MazeOutputStream = new ObjectOutputStream(outToClient);
+            int [] MazeDim = (int[])DimInputStream.readObject();
+            //Generate maze
             IMazeGenerator mg = new MyMazeGenerator();
             Maze GenMaze = mg.generate(MazeDim[0]/*Rows*/,MazeDim[1]/*Cols*/);
+            //declaring ByteOutputCompressStream as byte maze to send
             ByteArrayOutputStream ByteOutputCompressStream = new ByteArrayOutputStream();
-            MyCompressorOutputStream compreesed = new MyCompressorOutputStream(ByteOutputCompressStream);
-            compreesed.write(GenMaze.toByteArray());
-            outToClient.write(ByteOutputCompressStream.toByteArray());//i think???
-
-        } catch (IOException e) {
+            MyCompressorOutputStream compressed = new MyCompressorOutputStream(ByteOutputCompressStream);
+            compressed.write(GenMaze.toByteArray());
+            MazeOutputStream.writeObject(ByteOutputCompressStream.toByteArray());
+            MazeOutputStream.flush();
+        } catch (IOException | ClassNotFoundException e) {
         e.printStackTrace();
     }
 
